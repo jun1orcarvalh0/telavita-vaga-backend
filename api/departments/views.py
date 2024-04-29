3c07871df017c6ed0bb72721a26a906b93375c29
jun1orcarvalh0 <antoniomarcosdecj18@gmail.com>
2024-04-29 16:07:07 -0300

    feat: Implementing departments views.py

--- api/departments/views.py
+++ api/departments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from departments import models, serializers
 
-# Create your views here.
+class DepartmentView(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+    queryset = models.Department.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RetrieveDepartmentSerializer
+        elif self.action == 'create':
+            return serializers.DepartmentSerializer
+        return serializers.DepartmentSerializer
+    
+class EmployeeView(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+    queryset = models.Employee.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.RetrieveEmployeeSerializer
+        elif self.action == 'create':
+            return serializers.EmployeeSerializer
+        return serializers.EmployeeSerializer
