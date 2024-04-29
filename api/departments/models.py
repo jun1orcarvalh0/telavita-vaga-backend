cb592759f1b444746d19086ed5c2fecf49b34303
jun1orcarvalh0 <antoniomarcosdecj18@gmail.com>
2024-04-29 16:03:52 -0300

    feat: Implementing departments models.py

--- api/departments/models.py
+++ api/departments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    dependents = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.full_name
